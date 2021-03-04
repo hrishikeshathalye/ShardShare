9d742daf20f8f8432829b31e66c4a201e7952169
nachiket59 <pethenachiket5@gmail.com>
2021-03-05 01:38:12 +0530

    Task: added create new secret ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,85 @@
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from "react";
 // import logo from './logo.svg';
-import './App.css';
-import { Container,Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Home from "./components/Login/Landing/Landing"
-import Navbar from "./components/Navbar/Nav"
-import CreateSecretForm from './components/create_secret/create_secret'
-import {Link, BrowserRouter, Switch, Route, useHistory} from 'react-router-dom'
-import Auth from './components/Auth/Auth';
-import Dashboard from './components/Dashboard/Dashboard';
-import { useDispatch, useSelector } from 'react-redux';
-import { verify } from './api/index';
-
+import "./App.css";
+import { Container, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Home from "./components/Login/Landing/Landing";
+import Navbar from "./components/Navbar/Nav";
+import CreateSecretForm from "./components/create_secret/create_secret";
+import {
+  Link,
+  BrowserRouter,
+  Switch,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Dashboard from "./components/Dashboard/Dashboard";
+import { useDispatch, useSelector } from "react-redux";
+import { verify } from "./api/index";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
- 
 }));
-const renderRoutes = (isVerified)=>{
-  if(isVerified)
-  return(
-    <Switch>
-    <Route exact path="/" component={Home} /> 
-    <Route exact path="/dashboard" component={Dashboard} />
-    <Route exact path="/create_context" component={CreateSecretForm} />
-    <Route exact path="/auth" component={Dashboard} />
-    </Switch>
-  )
+const renderRoutes = (isVerified) => {
+  if (isVerified)
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/create_context" component={CreateSecretForm} />
+        <Route exact path="/auth" component={Dashboard} />
+      </Switch>
+    );
   else
-  return(
-    <Switch>
-    <Route exact path="/" component={Home} /> 
-    <Route exact path="/dashboard" component={Home} />
-    <Route exact path="/create_context" component={Home} />
-    <Route exact path="/auth" component={Home} />
-    </Switch>
-  ) 
-}
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Home} />
+        <Route exact path="/create_context" component={Home} />
+        <Route exact path="/auth" component={Home} />
+      </Switch>
+    );
+};
 function App() {
   const classes = useStyles();
-  let state = useSelector(state => state);
+  let state = useSelector((state) => state);
   const dispatch = useDispatch();
   let [isVerified, setVerified] = useState(null);
   useEffect(() => {
-    verify().then((res)=>{
-      setVerified(res.data.userId);
-    }).catch(()=>{
-      setVerified(null);
-    });
+    verify()
+      .then((res) => {
+        setVerified(res.data.userId);
+      })
+      .catch(() => {
+        setVerified(null);
+      });
   }, []);
   return (
     <BrowserRouter>
       <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-        <Navbar />
-        </Grid>
-        <Grid item xs={12}>
-        <Switch>
-          <Route exact path="/" component={Home} /> 
-          { isVerified ? 
-             <Route exact path="/dashboard" component={Dashboard}/>:
-            <Route exact path="/dashboard" component={Home} />
-          }
-          <Route exact path="/auth" component={Auth} />
-          <Route exact path="/create_secret" component={CreateSecretForm} />
-        </Switch>
-        </Grid>
-        
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Navbar />
+          </Grid>
+          <Grid item xs={12}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              {isVerified ? (
+                <Route exact path="/dashboard" component={Dashboard} />
+              ) : (
+                <Route exact path="/dashboard" component={Home} />
+              )}
+              <Route exact path="/auth" component={Auth} />
+              <Route exact path="/create_secret" component={CreateSecretForm} />
+            </Switch>
+          </Grid>
         </Grid>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,52 +1,64 @@
 import React from "react";
-import Nav from "../Navbar/Nav"
-import MasterCard from './Card.js'
-import {
-    Grid,
-   
-} from '@material-ui/core/'
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-      },
-      MasterCard: {
-        justify:'center',
-        padding: theme.spacing(4),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      },
-}))
+import Nav from "../Navbar/Nav";
+import MasterCard from "./Card.js";
+import { Grid } from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  MasterCard: {
+    justify: "center",
+    padding: theme.spacing(4),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 function Dashboard() {
-    const classes = useStyles()
-    return (
-        <div className={classes.root} >
-            <Grid container justify = "center" spacing = {3}>
-                <Grid item xs  >
-                    <div className={classes.MasterCard}>
-                    <MasterCard title="Create new Secret" description = "Add new secret, shard it, and share!" image="/images/lock1.png" link = '/create_secret'/>
-                    </div>
-                </Grid>
-                <Grid item xs >
-                <div className={classes.MasterCard}>
-                    <MasterCard title="Your secrets" description = "Seccrets that you created and shared" image="/images/confidential.png"/>
-                    </div>
-                </Grid>
-                <Grid item xs >
-                <div className={classes.MasterCard}>
-                    <MasterCard title="Secrets shared with you" description = "Secrets in which you are a participant" image="/images/key.png"/>
-                    </div>
-                </Grid>
-                <Grid item xs >
-                <div className={classes.MasterCard}>
-                    <MasterCard title="Recovery Requests" description = "Inbox of recovery requests" image="/images/key.png"/>
-                    </div>
-                </Grid>
-               </Grid>
-            </div>
-    );
-
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container justify="center" spacing={3}>
+        <Grid item xs>
+          <div className={classes.MasterCard}>
+            <MasterCard
+              title="Create new Secret"
+              description="Add new secret, shard it, and share!"
+              image="/images/lock1.png"
+              link="/create_secret"
+            />
+          </div>
+        </Grid>
+        <Grid item xs>
+          <div className={classes.MasterCard}>
+            <MasterCard
+              title="Your secrets"
+              description="Seccrets that you created and shared"
+              image="/images/confidential.png"
+            />
+          </div>
+        </Grid>
+        <Grid item xs>
+          <div className={classes.MasterCard}>
+            <MasterCard
+              title="Secrets shared with you"
+              description="Secrets in which you are a participant"
+              image="/images/key.png"
+            />
+          </div>
+        </Grid>
+        <Grid item xs>
+          <div className={classes.MasterCard}>
+            <MasterCard
+              title="Recovery Requests"
+              description="Inbox of recovery requests"
+              image="/images/key.png"
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Dashboard;
-
